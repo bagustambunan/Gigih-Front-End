7f27d02b279a2a4ecee5b68b103040e42ad2eb97
Muhammad Bagus Syahputra Tambunan <mhdbagus78@gmail.com>
2021-07-07 21:42:58 +0700

    Sesi 2

---
+++ modul-1/homework/js/index.js
@@ -0,0 +1,20 @@
+const form = document.getElementById('form');
+const text_title = document.getElementById('text_title');
+const text_artist = document.getElementById('text_artist');
+const text_album = document.getElementById('text_album');
+
+const handleSubmit = () => {
+    alert('Semangat selalu yang sedang berjuang!');
+}
+
+form.addEventListener('submit', handleSubmit);
+
+axios
+    .get("https://gist.githubusercontent.com/aryapradipta9/e6492383477803b233916e01f36d5465/raw/66942c739d66d3774303f84071696aa865a07077/single-sample.json", {})
+    .then(response => {
+        console.log(response.data);
+        text_title.innerHTML = response.data['album']['name'];
+        text_artist.innerHTML = response.data['album']['artists'][0]['name'];
+        text_album.innerHTML = response.data['name'];
+    })
+    .catch(err => alert(err.data));
--- modul-1/sesi-1/homework/js/index.js
+++
@@ -1,5 +0,0 @@
-const handleSubmit = () => {
-    alert('Semangat selalu yang sedang berjuang!');
-}
-
-document.getElementById('form').addEventListener('submit', handleSubmit);
