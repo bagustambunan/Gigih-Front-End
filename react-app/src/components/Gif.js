cd93a606496dcbe759b07e8c7fd10c5fb73435d0
Muhammad Bagus Syahputra Tambunan <mhdbagus78@gmail.com>
2021-07-15 22:32:01 +0700

    Add function to get images from search bar

--- react-app/src/components/Gif.js
+++ react-app/src/components/Gif.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Gif = props => {
     return (
-        <div className="justify-start bg-white w-4/12 p-3 border">
-            <img src={props.url} title={props.title} className="rounded-lg "/>
+        <div className="justify-start w-4/12 p-3">
+            <img src={props.url} title={props.title} className="rounded-lg " alt={props.title}/>
             <p className="text-sm mt-2 mb-2">{props.title}</p>
         </div>
     );
--- react-app/src/page/home/index.js
+++ react-app/src/page/home/index.js
@@ -1,38 +1,62 @@
 import React from 'react';
-import Form from '../../components/Form';
 import Gif from '../../components/Gif';
 
 const axios = require('axios');
+const giphy_key = process.env.REACT_APP_GIPHY_KEY;
 
 class Search extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            url_img: "https://media.giphy.com/media/Vh8pbGX3SGRwFDh3V0/source.gif",
-            url_giphy: "belum-ada"
+            // url_img: ["https://media.giphy.com/media/Vh8pbGX3SGRwFDh3V0/source.gif"],
+            data_img: [],
+            url_giphy: `https://api.giphy.com/v1/gifs/search?api_key=${giphy_key}&q=google&limit=3&offset=0&rating=g&lang=en`
         }
     }
 
-    render() {
-        const gif = {
-            id: '4HrBfVJJveBNS9ytSk',
-            title: 'Nintendo Plotting GIF by Gaming GIFs',
-            uploadedDate: '2018-04-03 15:21:50',
-            url: 'https://media4.giphy.com/media/4HrBfVJJveBNS9ytSk/200w.gif?cid=cb3f2bebpuo6jj0g5f9gfibjre2zzbb4yb1cfshtplanlrpw&rid=200w.gif&ct=g',
-            webp: 'https://media4.giphy.com/media/4HrBfVJJveBNS9ytSk/giphy.webp?cid=cb3f2bebpuo6jj0g5f9gfibjre2zzbb4yb1cfshtplanlrpw&rid=giphy.webp&ct=g',
+    async getImg() {
+        try {
+            const resp = await axios.get(this.state.url_giphy);
+            // console.log(resp.data.data);
+            this.setState({
+                data_img: resp.data.data,
+            });
+            console.log(this.state.data_img);
+        } catch (err) {
+            console.error(err);
         }
+    };
+
+    searchGiphy(event) {
+        let q = event.target.value;
+        let url_new = `https://api.giphy.com/v1/gifs/search?api_key=${giphy_key}&q=${q}&limit=3&offset=0&rating=g&lang=en`;
+        this.setState({
+            url_giphy: url_new,
+        });
+        this.getImg();
+    }
+
+    render() {
         
         return (
             <center className="p-5">
                 <div className="w-9/12 object-center">
-                    <Form/>
-                    <p>{this.state.url_giphy}</p>
-                    <div className="bg-gray-100 p-8 rounded-lg mt-5 flex">
-                        <Gif url={gif.url} title={gif.title}/>
-                        <Gif url={gif.url} title={gif.title}/>
-                        <Gif url={gif.url} title={gif.title}/>
+
+                    <div className="">
+                        <input onChange={(event) => {this.searchGiphy(event)}} type="text" name="q" className="bg-gray-200 px-4 py-3 rounded-lg w-full" placeholder="Type anything..."></input>
+                        {/* <p>{this.state.url_giphy}</p> */}
                     </div>
+                    
+                    <div className="bg-gray-100 p-8 rounded-lg mt-5 flex">
+                    {this.state.data_img.map((item) => {
+                        return (
+                            <Gif url={item.images.original.url} title={item.title}/>
+                        );
+                    })}
+
+                    </div>
+
                 </div>
             </center>
         );
